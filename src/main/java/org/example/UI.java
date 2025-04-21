6832481e7c2184a8e2b3dd0c6260616a13fe2ed1
Andreyka <andreykostygin@gmail.com>
2025-04-21 11:50:05 +0300

    Newcommitznovy

--- src/main/java/org/example/UI.java
+++ src/main/java/org/example/UI.java
@@ -21,7 +21,7 @@ public class UI extends JFrame implements MouseListener, KeyListener {
         setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
-        setResizable(true);
+        setResizable(false);
 
         add(gameZone);
 
